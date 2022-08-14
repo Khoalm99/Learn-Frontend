58aa7a16abb83a2fd1e2a60d6b73c8d97430fae9
Khoalm99 <khoaleminh1211@gmail.com>
2022-08-14 14:39:34 +0700

    update static

--- index.js
+++ index.js
@@ -1,23 +1,8 @@
-
-var typed = new Typed(".typing-1",{
+var typed = new Typed(".typing",{
     strings: [
-        "Lê Minh Khoa"
+        "I'm Minh Khoa"
     ],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true
-});
-var typed = new Typed(".typing-2",{
-    strings: [
-        "YouTuber", 
-        "Lê Minh Khoa", "Freelancer"
-    ],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true
-});
-
-//slide-up script
-$('.scroll-up-btn').click(function(){
-    $('html').animate({scrollTop:0});
 });
